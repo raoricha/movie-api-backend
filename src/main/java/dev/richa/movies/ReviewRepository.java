0b1d8783f015ea717315a2634a65e77c1c9c7d58
Richa Rao <raoricha115@gmail.com>
2023-02-25 20:36:53 +0530

    first commit

---
+++ src/main/java/dev/richa/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.richa.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.MongoAdminOperations;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
